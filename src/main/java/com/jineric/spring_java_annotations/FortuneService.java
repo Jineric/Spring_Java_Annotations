9df30dd5bed41c0d7bdbc077bc94c8c1786d7c74
Eric Baek <eeb0129@ATL42EEB071221.local>
2019-04-24 13:16:04 -0400

    added autowired with constructor injection

---
+++ src/main/java/com/jineric/spring_java_annotations/FortuneService.java
@@ -0,0 +1,7 @@
+package com.jineric.spring_java_annotations;
+
+public interface FortuneService {
+
+    public String getFortune();
+
+}
--- src/main/java/com/jineric/spring_java_annotations/TennisCoach.java
+++ src/main/java/com/jineric/spring_java_annotations/TennisCoach.java
@@ -1,9 +1,23 @@
 package com.jineric.spring_java_annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component //default name is tennisCoach
+//@Component("theSillyCoach") --for naming the spring bean
 public class TennisCoach implements Coach {
+    private FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice backhand volley";
